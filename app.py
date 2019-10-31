c7a42d90cb88f16c56f54574ab05f7d6ad110211
myname21-meet <myname21@meet.mit.edu>
2019-10-31 20:03:40 +0200

    mini project

--- app.py
+++ app.py
@@ -5,8 +5,24 @@ app = Flask(__name__)
 app.secret_key = "MY_SUPER_SECRET_KEY"
 
 
-##### Code here ######
 
+##### Code here ######
+@app.route('/')
+def home():
+	return render_template("home.html")
+
+
+@app.route('/store')
+def store():
+	return render_template("store.html")
+
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+@app.route('/cart')
+def cart():
+	return render_template("cart.html")
 
 
 #####################
